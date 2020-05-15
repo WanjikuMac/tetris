31bc2127c8aeb949a8700ab8442f607fe19c460c
WanjikuMac <edwardscaroline32@gmail.com>
2020-05-15 03:15:50 +0300

    modify point to be easily piped

--- lib/point.ex
+++ lib/point.ex
@@ -1,4 +1,21 @@
-defmodule Tetris.Point do
+defmodule Point do
+    #This ensures that we get an x and a y point
+    @enforce_keys [:x, :y]
+    defstruct x: 0, y: 0
+
+    def new(x,y) when is_integer(x) and is_integer(y) do
+       %Point{x: x, y: y}
+    end
+
+    def right1(%Point{x: x, y: y}) when is_integer(x) and is_integer(y) do
+        %Point{x: x +1, y: y}
+     end
+
+    # def left1(%Point{x: x, y: y}) when is_integer(x) and is_integer(y) do
+    #     %Point{x: x, y: y-1}
+    # end
+
+    # Points with Tuple data structure   
     def right({x, y}) do
     {x + 1, y}
     end
